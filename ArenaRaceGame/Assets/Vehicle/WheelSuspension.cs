fdec889210bf9bc4c4180584f4f66902fb33af11
Greg Weil <gregweil@pentavera.com>
2016-07-28 20:46:15 -0400

    Basic wheel suspensions that moves to match the ground

---
+++ ArenaRaceGame/Assets/Vehicle/WheelSuspension.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelSuspension : MonoBehaviour {
+
+    public float range = 1f;
+    public float radius = 0.5f;
+    
+    Rigidbody body = null;
+    GameObject collide = null;
+
+    Transform wheel = null;
+
+    float position = 0f;
+
+    // Use this for initialization
+    void Start () {
+        wheel = transform.GetChild(0);
+        body = GetComponentInParent<Rigidbody>();
+        collide = body.GetComponentInChildren<Collider>().gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        collide.SetActive(false);
+
+        RaycastHit hit;
+        if (Physics.CheckSphere(transform.position, radius)) {
+            position = 0;
+        } else if (Physics.SphereCast(transform.position, radius, -transform.up, out hit, range)) {
+            position = hit.distance;
+        } else {
+            position = range;
+        }
+
+        position = Mathf.Clamp(position, 0f, range);
+
+        wheel.localPosition = new Vector3(wheel.localPosition.x, -position, wheel.localPosition.z);
+
+        collide.SetActive(true);
+	}
+}
