9fc936a13485d46c366b8c02371855edb4dbf2e6
Greg Weil <gregweil@pentavera.com>
2016-07-31 18:33:13 -0400

    Interpolation for WheelCollider models

--- ArenaRaceGame/Assets/Vehicle/WheelMesh.cs
+++ ArenaRaceGame/Assets/Vehicle/WheelMesh.cs
@@ -5,17 +5,34 @@ public class WheelMesh : MonoBehaviour {
 
     public WheelCollider trackCollider;
 
+    Vector3 positionLast, position, positionSpeed;
+    Quaternion rotationLast, rotation;
+    float timeIncrement = 0f;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        trackCollider.GetWorldPose(out positionLast, out rotationLast);
+        trackCollider.GetWorldPose(out position, out rotation);
+        positionSpeed = Vector3.zero;
+        timeIncrement = 0f;
+    }
+
+    // FixedUpdate is caled every physics step
+    void FixedUpdate () {
+        positionLast = position;
+        rotationLast = rotation;
+        trackCollider.GetWorldPose(out position, out rotation);
+        position = transform.parent.InverseTransformPoint(position);
+        rotation = Quaternion.Inverse(transform.parent.rotation) * rotation;
+        timeIncrement = Time.fixedDeltaTime;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 position;
-        Quaternion rotation;
-        trackCollider.GetWorldPose(out position, out rotation);
-        transform.position = position;
-        transform.rotation = rotation;
-	}
+        timeIncrement += Time.deltaTime;
+        var t = (timeIncrement / Time.fixedDeltaTime);
+        var targetPosition = Vector3.LerpUnclamped(positionLast, position, t);
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, targetPosition, ref positionSpeed, 0.025f);
+        transform.rotation = Quaternion.SlerpUnclamped(rotationLast, rotation, t);
+    }
 }
